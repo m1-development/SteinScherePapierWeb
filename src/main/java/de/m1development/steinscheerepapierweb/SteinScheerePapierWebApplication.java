754249ffe6afd29eb66e0b82cc2e6f030a6f0162
m1-development <dennis.public@gmx.net>
2025-06-02 15:28:50 +0200

    Initial commit

---
+++ src/main/java/de/m1development/steinscheerepapierweb/SteinScheerePapierWebApplication.java
@@ -0,0 +1,13 @@
+package de.m1development.steinscheerepapierweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteinScheerePapierWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SteinScheerePapierWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/m1development/steinscheerepapierweb/SteinScheerePapierWebApplicationTests.java
@@ -0,0 +1,13 @@
+package de.m1development.steinscheerepapierweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteinScheerePapierWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
